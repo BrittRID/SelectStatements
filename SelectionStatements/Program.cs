15da59e366fbb82730b0374d2831f21cc049dcfe
Brittany Ridley <britt.ridley@outlook.com>
2022-03-21 22:34:15 -0500

    Select

---
+++ SelectionStatements/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace SelectionStatements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //int favNumber = 7;
+            //Console.WriteLine("Guess my favorite number. Hint: Below 10");
+            //int userguess = int.Parse(Console.ReadLine());
+
+            // if (userguess < favNumber)
+            //{
+            //   Console.WriteLine("TOO LOW!");
+            //}
+            // else if (userguess > favNumber)
+            // {
+            //      Console.WriteLine("TOO HIGH");
+            // }
+            // else if (userguess == favNumber)
+            //{
+            //    Console.WriteLine("You were correct here is a prize");
+            //}
+            // else
+            // {
+            //    Console.WriteLine("Nevermind");
+            // }
+            string subject = "c#";
+            Console.WriteLine("Choose a subject");
+            string userSubject = Console.ReadLine();
+
+            switch (userSubject)
+            {
+
+                case "c#":
+                    Console.WriteLine("You are in the right class");
+                    break;
+                case "Jave":
+                    Console.WriteLine("c# is better than jave");
+                    break;
+
+                default:
+                    Console.WriteLine("What is the game plan sister!!!!");
+                    break;
+
+
+
+
+
+            }
+
+
+        }
+    }
+}
